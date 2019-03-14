0dc29297c6b352117bcbe23c1ad0b6d5a62c5f82
Emile su <emilesu@qq.com>
2019-03-14 17:43:47 +0800

    Update wechat_mobile.rb

--- lib/omniauth/strategies/wechat_mobile.rb
+++ lib/omniauth/strategies/wechat_mobile.rb
@@ -2,7 +2,7 @@ require "omniauth-oauth2"
 
 module OmniAuth
   module Strategies
-    class WechatMobile < OmniAuth::Strategies::OAuth2
+    class Wechat < OmniAuth::Strategies::OAuth2
       option :name, "wechat_mobile"
 
       option :client_options, {
